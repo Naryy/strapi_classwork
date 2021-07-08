1170f3e043f86998f63bd07169af0357aaa70e52
“Naryy” <narikawanariyuki@gmail.com>
2021-07-08 18:44:50 +0900

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0739310e31f15538a41ebd830e346971'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'adbf6479-378b-4567-8d13-3d25b5654c4e'
+};
